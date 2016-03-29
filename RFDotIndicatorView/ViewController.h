9eb313dcca6bea738bbacf9158418d5285ff8d4c
包捷勇 <infinty.bao@gmail.com>
2016-03-29 19:09:04 +0800

    Initial Commit

---
+++ RFDotIndicatorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RFDotIndicatorView
+//
+//  Created by 包捷勇 on 16/3/29.
+//  Copyright © 2016年 包捷勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RFDotIndicatorView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RFDotIndicatorView
+//
+//  Created by 包捷勇 on 16/3/29.
+//  Copyright © 2016年 包捷勇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
